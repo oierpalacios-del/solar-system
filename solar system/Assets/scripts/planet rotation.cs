17246798fe5526ee9c55b41c10c77e245e57370c
oierpalacios-del <oier.palacios@opendeusto.es>
2026-04-12 14:09:44 +0200

    a

--- solar system/Assets/scripts/planet rotation.cs
+++ solar system/Assets/scripts/planet rotation.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class planetrotation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float rotationSpeed;
+    public GameObject sun;
     void Start()
     {
-        
+        rotationSpeed = 10f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(gameObject.name == "sol")
+        {
+            transform.rotation = Quaternion.Euler((transform.rotation.x + rotationSpeed) * Time.deltaTime, (transform.rotation.y + rotationSpeed) * Time.deltaTime, (transform.rotation.z + rotationSpeed) * Time.deltaTime);
+        }
+        else
+        {
+            float distance = Vector3.Distance(transform.position, sun.transform.position);
+            transform.rotation = Quaternion.Euler((transform.rotation.x - distance + rotationSpeed) * Time.deltaTime, (transform.rotation.y - distance + rotationSpeed) * Time.deltaTime, (transform.rotation.z - distance + rotationSpeed) * Time.deltaTime);
+        }
     }
 }
